b5db3a85f8ae3fc9a0c8f5dc77c9217c474b7e3d
Harshit-Sisodia <harshitsisodia789@gmail.com>
2024-05-01 23:52:06 +0530

    update

---
+++ patterns/solidRectangle.java
@@ -0,0 +1,24 @@
+package patterns;
+
+import java.util.*;
+
+public class solidRectangle {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the lenght of rectangle:");
+        int l= sc.nextInt();
+        System.out.println("Enter the length of rectangle:");
+        int  b= sc.nextInt();
+
+        for(int i = 0; i<b ; i++)
+        {
+            for (int j = 0; j < l; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
