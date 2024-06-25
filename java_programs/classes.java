5180051188e255316150e4d9dd9c79f494310b70
Harshit-Sisodia <harshitsisodia789@gmail.com>
2024-06-25 22:17:53 +0530

    update

--- java_programs/classes.java
+++ java_programs/classes.java
@@ -1,10 +1,36 @@
 package java_programs;
 
+class calculate {
+
+    public int add(int a, int b)     // method for adding
+    {
+        int result = a + b;
+
+        // System.out.println("add the value "+result);
+        return result;
+    }
+}
+
 public class classes {
 
     public static void main(String[] args) {
 
-        
+        int a = 54;
+        int b = 34;
+
+        // int result = a+b;
+
+        // System.out.println(result);
+
+        // making an object
+
+        int result;
+
+
+        calculate calc = new calculate();
+        result = calc.add(a,b);
+
+        System.out.println("The value of add is "+result);
         
     }
     
